bbfae8ae60213d3d18581b99b733afc04f851237
mefuru <mehul.mandania@gmail.com>
2019-03-22 14:16:50 +0100

    Updates

---
+++ admin/0.a96bcc23447765e819ab.hot-update.js
@@ -0,0 +1,142 @@
+webpackHotUpdate(0,{
+
+/***/ 32:
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* WEBPACK VAR INJECTION */(function(module) {/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(9);
+/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(react__WEBPACK_IMPORTED_MODULE_0__);
+var _jsxFileName = "/Users/mehulmandania/Dropbox/sandbox/mmandania/src/cms/preview-templates/post-preview.js";
+
+(function () {
+  var enterModule = __webpack_require__(7).enterModule;
+
+  enterModule && enterModule(module);
+})();
+
+
+
+var PostPreview = function PostPreview(_ref) {
+  var entry = _ref.entry,
+      widgetFor = _ref.widgetFor;
+  var body = widgetFor('body');
+  var title = entry.getIn(['data', 'title']);
+  return react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("div", {
+    className: "post",
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 8
+    },
+    __self: this
+  }, react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("h1", {
+    className: "post__title",
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 9
+    },
+    __self: this
+  }, title), react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("div", {
+    className: "post__body",
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 10
+    },
+    __self: this
+  }, body));
+};
+
+var _default = PostPreview;
+/* harmony default export */ __webpack_exports__["default"] = (_default);
+;
+
+(function () {
+  var reactHotLoader = __webpack_require__(7).default;
+
+  var leaveModule = __webpack_require__(7).leaveModule;
+
+  if (!reactHotLoader) {
+    return;
+  }
+
+  reactHotLoader.register(PostPreview, "PostPreview", "/Users/mehulmandania/Dropbox/sandbox/mmandania/src/cms/preview-templates/post-preview.js");
+  reactHotLoader.register(_default, "default", "/Users/mehulmandania/Dropbox/sandbox/mmandania/src/cms/preview-templates/post-preview.js");
+  leaveModule(module);
+})();
+
+;
+/* WEBPACK VAR INJECTION */}.call(this, __webpack_require__(6)(module)))
+
+/***/ }),
+
+/***/ 5:
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* WEBPACK VAR INJECTION */(function(module) {/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(9);
+/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(react__WEBPACK_IMPORTED_MODULE_0__);
+var _jsxFileName = "/Users/mehulmandania/Dropbox/sandbox/mmandania/src/cms/preview-templates/page-preview.js";
+
+(function () {
+  var enterModule = __webpack_require__(7).enterModule;
+
+  enterModule && enterModule(module);
+})();
+
+
+
+var PagePreview = function PagePreview(_ref) {
+  var entry = _ref.entry,
+      widgetFor = _ref.widgetFor;
+  var body = widgetFor('body');
+  var title = entry.getIn(['data', 'title']);
+  return react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("div", {
+    className: "page",
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 8
+    },
+    __self: this
+  }, react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("h1", {
+    className: "page__title",
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 9
+    },
+    __self: this
+  }, title), react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("div", {
+    className: "page__body",
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 10
+    },
+    __self: this
+  }, body));
+};
+
+var _default = PagePreview;
+/* harmony default export */ __webpack_exports__["default"] = (_default);
+;
+
+(function () {
+  var reactHotLoader = __webpack_require__(7).default;
+
+  var leaveModule = __webpack_require__(7).leaveModule;
+
+  if (!reactHotLoader) {
+    return;
+  }
+
+  reactHotLoader.register(PagePreview, "PagePreview", "/Users/mehulmandania/Dropbox/sandbox/mmandania/src/cms/preview-templates/page-preview.js");
+  reactHotLoader.register(_default, "default", "/Users/mehulmandania/Dropbox/sandbox/mmandania/src/cms/preview-templates/page-preview.js");
+  leaveModule(module);
+})();
+
+;
+/* WEBPACK VAR INJECTION */}.call(this, __webpack_require__(6)(module)))
+
+/***/ })
+
+})
+//# sourceMappingURL=0.a96bcc23447765e819ab.hot-update.js.map
--- content/pages/contacts.md
+++
@@ -1,6 +0,0 @@
----
-title: "Contact me"
-template: "page"
----
-
-Contact me at mehul [dot] mandania [at] gmail [dot] com
